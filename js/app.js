2f1bfc6dce7ea8d3d9d7d088d16ef75cb4e3f4c3
GoOshima <goshima5@gmail.com>
2015-03-15 11:14:39 +0900

    prototype for development
    prototype for development

---
+++ js/app.js
@@ -0,0 +1,68 @@
+window.addEventListener("load", function () {
+    console.log("app loaded...");
+});
+
+$(document).ready(function () {
+
+    var pageId = $('body').attr("id");
+
+    //タイトル表示
+    if (pageId == 'top') {
+        startTittleAnim();
+        $('#gotStampBtn').click(function () {
+            window.location.href = "stamp2.html";
+        });
+    }
+
+    if (pageId == 'stamp') {
+        //stampページ 1
+        showIcon('#testIcon1', 1000);
+        showIcon('#testIcon2', 4000);
+
+        //stampページ 2
+        showIcon('#testIcon1_2', 1000);
+        showIcon('#testIcon2_2', 4000);
+    }
+
+});
+
+//アイコン表示
+function showIcon(argId, argTime) {
+    setTimeout(function () {
+        $(argId).show();
+        audio.play();
+    }, argTime);
+}
+
+//TODO タイトル表示
+function startTittleAnim() {
+    var targetObj = $('.split');
+    var delaySpeed = 200;
+    var fadeSpeed = 0;
+    var targetTxt = targetObj.html();
+
+    targetObj.css({visibility: 'visible'}).children().addBack().contents().each(function () {
+        var elmThis = $(this);
+        if (this.nodeType == 3) {
+            var $this = $(this);
+            $this.replaceWith($this.text().replace(/(\S)/g, '<span class="textSplitLoad">$&</span>'));
+        }
+    });
+    splitLength = $('.textSplitLoad').length;
+    targetObj.find('.textSplitLoad').each(function (i) {
+        splitThis = $(this);
+
+        splitTxt = splitThis.text();
+
+        splitThis.delay(i * (delaySpeed)).css({
+            display: 'inline-block',
+            opacity: '0'
+        }).animate({opacity: '1'}, fadeSpeed);
+    });
+
+    setTimeout(function () {
+        targetObj.html(targetTxt);
+        $('#mainTitle').fadeOut('slow');
+        $('#mainBox').fadeIn('slow');
+    }, splitLength * delaySpeed + fadeSpeed);
+}
